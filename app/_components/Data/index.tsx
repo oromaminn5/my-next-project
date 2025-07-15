e72308262e5c5ebb46bc2ba4321e3ccd4b807e21
masanori kataoka <masanorikataoka@Mac-Studio.local>
2025-07-15 12:16:19 +0900

    6章まで完了

--- app/_components/Data/index.tsx
+++ app/_components/Data/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./index.module.css";
+import { formatDate } from "@/app/_libs/utils";
 
 type Props = {
   data: string;
@@ -9,7 +10,7 @@ export default function Data({ data }: Props) {
   return (
     <span className={styles.data}>
       <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
-      {data}
+      {formatDate(data)}
     </span>
   );
 }
