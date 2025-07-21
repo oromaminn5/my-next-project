6071e9048796cc087ea037d4b17a2aaef6ddf456
masanori kataoka <masanorikataoka@Mac-Studio.local>
2025-07-22 03:17:53 +0900

    7章まで完了

--- app/_components/Category/index.tsx
+++ app/_components/Category/index.tsx
@@ -4,7 +4,6 @@ import styles from "./index.module.css";
 type Props = {
   category: Category;
 };
-
 export default function Category({ category }: Props) {
   return <span className={styles.tag}>{category.name}</span>;
 }
