6df773c44cead247a28e80d2a0a7070be4bae8e9
masanori kataoka <masanorikataoka@Mac-Studio.local>
2025-07-22 03:55:35 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import Pagination from "../_components/Pagenation";
 import SearchField from "../_components/searchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
